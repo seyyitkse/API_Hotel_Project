2540bca4fd00169b993cfc7a11503c2bd818e9fa
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-20 20:20:21 +0300

    Necessary files have been added to the project layers.

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/StaffManager.cs
@@ -0,0 +1,45 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class StaffManager : IStaffService
+    {
+        private readonly IStaffService _staffService;
+
+        public StaffManager(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+
+        public void TDelete(Staff entity)
+        {
+            _staffService.TDelete(entity);
+        }
+
+        public Staff TGetById(int id)
+        {
+            return _staffService.TGetById(id);
+        }
+
+        public List<Staff> TGetList()
+        {
+            return _staffService.TGetList();
+        }
+
+        public void TInsert(Staff entity)
+        {
+            _staffService.TInsert(entity);
+        }
+
+        public void TUpdate(Staff entity)
+        {
+            _staffService.TUpdate(entity);
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/TestimonialManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class TestimonialManager : ITestimonialService
+    {
+        private readonly ITestimonialDal _testimonialDal;
+
+        public TestimonialManager(ITestimonialDal testimonialDal)
+        {
+            _testimonialDal = testimonialDal;
+        }
+
+        public void TDelete(Testimonial entity)
+        {
+            _testimonialDal.Delete(entity);
+        }
+
+        public Testimonial TGetById(int id)
+        {
+            return _testimonialDal.GetById(id);
+        }
+
+        public List<Testimonial> TGetList()
+        {
+            return _testimonialDal.GetList();
+        }
+
+        public void TInsert(Testimonial entity)
+        {
+            _testimonialDal.Delete(entity);
+        }
+
+        public void TUpdate(Testimonial entity)
+        {
+            _testimonialDal.Update(entity);
+        }
+    }
+}
