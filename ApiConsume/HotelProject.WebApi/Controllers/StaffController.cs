0916e5937b37d60b2ae2dc94d4653c3c1b8fee87
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-21 21:09:19 +0300

    API controllers started to be added.

---
+++ ApiConsume/HotelProject.WebApi/Controllers/StaffController.cs
@@ -0,0 +1,45 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly IStaffService _staffService;
+
+        public StaffController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+
+        [HttpGet]
+        public IActionResult StaffList()
+        {
+            var values=_staffService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddStaff()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteStaff()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateStaff()
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStaff()
+        {
+            return Ok();
+        }
+    }
+}
