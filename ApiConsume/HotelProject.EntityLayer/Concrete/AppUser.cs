8889b552d8b741a22acbe717761715c55c620285
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-06-12 11:15:23 +0300

    Updated to ASP.NET Core Identity for user management
    Updated the `Context` class to inherit from `IdentityDbContext` for user management. Added necessary packages for Identity framework to various project files. Updated `ContextModelSnapshot.cs` to include `AppRole` and `AppUser` entities and configurations. Removed `Class1.cs` from `HotelProject.DtoLayer`. Added new migration file `20240612073000_add_identity_table.cs` for creating Identity tables in the database. Added `AppRole.cs` and `AppUser.cs` files defining classes for roles and users in the system.

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class AppUser:IdentityUser<int>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+    }
+}
