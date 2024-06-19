327bef23efb43693653c1e06f2da7730ad36b552
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-06-19 22:38:10 +0300

    Add and update testimonial features
    - Introduced `TestimonialController` with actions for CRUD operations on testimonials, including fetching from an external API, adding, deleting, and updating testimonials.
    - Added `TestimonialViewModel` with necessary properties for the testimonial data.
    - Created `Index.cshtml` for displaying testimonials with options for CRUD operations.
    - Updated `_ViewImports.cshtml` to include the `TestimonialViewModel` across all Razor views.

---
+++ Frontend/HotelProject.WebUI/Controllers/TestimonialController.cs
@@ -0,0 +1,85 @@
+﻿using HotelProject.WebUI.Models.Staff;
+using HotelProject.WebUI.Models.Testimonial;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestimonialController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5077/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<TestimonialViewModel>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddTestimonial(TestimonialViewModel newTestimonial)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(newTestimonial);
+            StringContent jsonTestimonial = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5077/api/Testimonial", jsonTestimonial);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:5077/api/Testimonial?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5077/api/Testimonial/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<TestimonialViewModel>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateTestimonial(TestimonialViewModel updateTestimonial)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateTestimonial);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "Application/Json");
+            var responseMessage = await client.PutAsync("http://localhost:5077/api/Staff/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
