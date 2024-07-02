69eed692de452b7063110dbf76cce2bb55d4931e
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-07-02 08:52:00 +0300

    Expand DTO layer and add new DTO classes
    Added references to new DTO folders in HotelProject.DtoLayer.csproj for SubscribeDto, StaffDto, and ServiceDto, indicating an expansion of the project's DTO layer to support new features. Introduced RoomAddDto in HotelProject.DtoLayer.RoomDto for adding new rooms, including necessary attributes for validation. Added UpdateRoomDto in the same namespace for updating room details, with validation attributes. Created a placeholder TestimonialAddDto class in HotelProject.DtoLayer.TestimonialDto for future testimonial feature development.

---
+++ ApiConsume/HotelProject.DtoLayer/RoomDto/RoomAddDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DtoLayer.RoomDto
+{
+    public class RoomAddDto
+    {
+        [Required(ErrorMessage = "Lütfen oda numarasını giriniz.")]
+        public string? RoomNumber { get; set; }
+        public string? RoomCoverImage { get; set; }
+        [Required(ErrorMessage = "Lütfen fiyat bilgisi giriniz.")]
+        public int Price { get; set; }
+        [Required(ErrorMessage = "Lütfen oda başlığını giriniz.")]
+        public string? Title { get; set; }
+        [Required(ErrorMessage = "Lütfen yatak sayısını giriniz.")]
+        public string? BedCount { get; set; }
+        [Required(ErrorMessage = "Lütfen banyo sayısını giriniz.")]
+        public string? BathCount { get; set; }
+        public string? Wifi { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ ApiConsume/HotelProject.DtoLayer/RoomDto/UpdateRoomDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DtoLayer.RoomDto
+{
+    public class UpdateRoomDto
+    {
+        public int RoomID { get; set; }
+        [Required(ErrorMessage = "Lütfen oda numarasını giriniz.")]
+        public string? RoomNumber { get; set; }
+        [Required(ErrorMessage = "Lütfen oda kapak resmini giriniz.")]
+        public string? RoomCoverImage { get; set; }
+        [Required(ErrorMessage = "Lütfen fiyat bilgisi giriniz.")]
+        public int Price { get; set; }
+        [Required(ErrorMessage = "Lütfen oda başlığını giriniz.")]
+        [StringLength(100, ErrorMessage = "Oda başlığı en fazla 100 karakter olabilir.")]
+        public string? Title { get; set; }
+        [Required(ErrorMessage = "Lütfen yatak sayısını giriniz.")]
+        public string? BedCount { get; set; }
+        [Required(ErrorMessage = "Lütfen banyo sayısını giriniz.")]
+        public string? BathCount { get; set; }
+        public string? Wifi { get; set; }
+        [Required(ErrorMessage = "Lütfen açıklama giriniz.")]
+        public string? Description { get; set; }
+    }
+}
