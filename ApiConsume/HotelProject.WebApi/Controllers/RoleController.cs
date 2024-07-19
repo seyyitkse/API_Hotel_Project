05819059cf0e0be9bd61801d593d1dad20102929
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-07-19 19:44:59 +0300

    Add RoleController to WebApi project
    - Introduced namespaces `Microsoft.AspNetCore.Http` and `Microsoft.AspNetCore.Mvc` for ASP.NET Core functionality.
    - Created a new namespace `HotelProject.WebApi.Controllers` and added the `RoleController` class within it.
    - Decorated `RoleController` with routing (`[Route("api/[controller]")]`) and `[ApiController]` attributes for API behaviors.
    - Inherited `RoleController` from `ControllerBase` to enable handling HTTP requests without view support.

---
+++ ApiConsume/HotelProject.WebApi/Controllers/RoleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+    }
+}
