17ae18b68c0bd807139c82f319f03844cbc0c598
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-08-10 20:25:51 +0300

    Add Star column to Rooms table and update entity model
    Introduce a new non-nullable int column 'Star' to the Rooms table
    with a default value of 0. Update the Room entity model and
    ContextModelSnapshot to include the new Star property. Create a
    migration file to handle the addition and removal of the Star
    column in the database schema.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20240806094334_update_room_table.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelProject.DataAccessLayer.Migrations
+{
+    public partial class update_room_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Star",
+                table: "Rooms",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Star",
+                table: "Rooms");
+        }
+    }
+}
--- ApiConsume/HotelProject.EntityLayer/Concrete/Room.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Room.cs
@@ -19,5 +19,6 @@ namespace HotelProject.EntityLayer.Concrete
         public string? BathCount { get; set; }
         public string? Wifi { get; set; }
         public string? Description { get; set; }
+        public int Star { get; set; }
     }
 }
