49cf22734cc7c31154d18dee9ab26bd86f08d57f
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-08-17 23:02:22 +0300

    Add Image column to Staffs table
    A new migration file named `20240813071206_add_image_column_with_staff_table.cs` has been added. This migration introduces a new column named `Image` to the `Staffs` table in the database. The `Image` column is of type `nvarchar(max)` and is nullable. The `Up` method in the migration adds the `Image` column to the `Staffs` table, while the `Down` method removes it, allowing for rollback if needed.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20240813071206_add_image_column_with_staff_table.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelProject.DataAccessLayer.Migrations
+{
+    public partial class add_image_column_with_staff_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Staffs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Staffs");
+        }
+    }
+}
