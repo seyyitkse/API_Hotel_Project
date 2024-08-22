afc6bae6862e3ec9116b75e1a197c5400b5e61c7
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-08-22 23:57:17 +0300

    Add Image property to Staff class for storing images
    The code change adds a new property `Image` of type `string?` to the `Staff` class within the `HotelProject.EntityLayer.Concrete` namespace. This property is intended to store an image associated with a staff member, allowing the application to display staff photos or other related images.

--- ApiConsume/HotelProject.EntityLayer/Concrete/Staff.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Staff.cs
@@ -16,5 +16,6 @@ namespace HotelProject.EntityLayer.Concrete
         public string? SocialMedia1 { get; set; }
         public string? SocialMedia2 { get; set; }
         public string? SocialMedia3 { get; set; }
+        public string? Image { get; set; }
     }
 }
