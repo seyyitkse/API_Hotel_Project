c515f62da70fe5862de4e2aab110d825439434a4
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-09-11 12:03:24 +0300

    Add feature to display top 100 IMDB movies
    Introduced a new feature to fetch and display a list of top 100 IMDB movies from a RapidAPI endpoint.
    
    - **DefaultController.cs**:
      - Added necessary `using` directives.
      - Created `DefaultController` class with `Index` action method.
      - Implemented API call to fetch movie data and deserialization.
    
    - **ImdbViewModel.cs**:
      - Added `ImdbViewModel` class with properties for movie details.
    
    - **Index.cshtml**:
      - Set model to list of `ImdbViewModel`.
      - Styled view with Bootstrap CSS.
      - Created table to display movie details.

---
+++ ApiConsume/RapidAPI/RapidAPI.Consume/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidAPI.Consume.Models;
+namespace RapidAPI.Consume.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public async Task< IActionResult> Index()
+        {
+            List<ImdbViewModel> imdbList = new List<ImdbViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com"),
+                Headers =
+            {
+                { "x-rapidapi-key", "9b86eb187emsh4ff8019e6473f3ep135f00jsnfc55dd15b8a3" },
+                { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+            },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                imdbList=JsonConvert.DeserializeObject<List<ImdbViewModel>>(body);
+                return View(imdbList);
+            }
+            return View();
+        }
+    }
+}
