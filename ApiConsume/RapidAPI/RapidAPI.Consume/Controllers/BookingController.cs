d99c417143c8e0426f410ae0aef4e49dff27d146
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-09-13 17:14:39 +0300

    Add BookingController, ViewModel, and Index view for hotels
    Added a new `BookingController` class in `BookingController.cs` to fetch hotel data from the Booking.com API using RapidAPI and display it in a view.
    
    Introduced a new model `BookingRootobject` and its related classes in `BookingViewModel.cs` to represent the structure of the data returned by the Booking.com API.
    
    Created a new view `Index.cshtml` to display a list of hotels with their names and review scores in a table format.

---
+++ ApiConsume/RapidAPI/RapidAPI.Consume/Controllers/BookingController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidAPI.Consume.Models;
+
+namespace RapidAPI.Consume.Controllers
+{
+    public class BookingController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels?dest_id=-741919&search_type=CITY&arrival_date=2024-09-14&departure_date=2024-09-16&adults=1&children_age=0%2C17&room_qty=1&page_number=1&units=metric&temperature_unit=c&languagecode=en-us&currency_code=AED"),
+                Headers =
+            {
+                { "x-rapidapi-key", "ba6a2cf21amsh6fbebb56a934146p159edcjsn6de3df7fee09" },
+                { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+            },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var bookingData = JsonConvert.DeserializeObject<BookingRootobject>(body);
+                return View(bookingData);
+            }
+        }
+    }
+}
---
+++ ApiConsume/RapidAPI/RapidAPI.Consume/Models/BookingViewModel.cs
@@ -0,0 +1,193 @@
+﻿namespace RapidAPI.Consume.Models
+{
+        public class BookingRootobject
+    {
+            public bool status { get; set; }
+            public string message { get; set; }
+            public long timestamp { get; set; }
+            public BookingData data { get; set; }
+        }
+
+        public class BookingData
+        {
+            public Hotel[] hotels { get; set; }
+            public Meta[] meta { get; set; }
+            public Appear[] appear { get; set; }
+        }
+
+        public class Hotel
+        {
+            public int hotel_id { get; set; }
+            public string accessibilityLabel { get; set; }
+            public Property1 property { get; set; }
+        }
+
+        public class Property1
+        {
+            public string reviewScoreWord { get; set; }
+            public int reviewCount { get; set; }
+            public float latitude { get; set; }
+            public int position { get; set; }
+            public Pricebreakdown priceBreakdown { get; set; }
+            public string[] blockIds { get; set; }
+            public int rankingPosition { get; set; }
+            public int id { get; set; }
+            public string checkinDate { get; set; }
+            public int mainPhotoId { get; set; }
+            public bool isFirstPage { get; set; }
+            public Checkout checkout { get; set; }
+            public string countryCode { get; set; }
+            public int accuratePropertyClass { get; set; }
+            public string checkoutDate { get; set; }
+            public float reviewScore { get; set; }
+            public float longitude { get; set; }
+            public string[] photoUrls { get; set; }
+            public int qualityClass { get; set; }
+            public Checkin checkin { get; set; }
+            public int ufi { get; set; }
+            public int optOutFromGalleryChanges { get; set; }
+            public string wishlistName { get; set; }
+            public string name { get; set; }
+            public int propertyClass { get; set; }
+            public string currency { get; set; }
+            public bool isExtended { get; set; }
+            public bool isPreferred { get; set; }
+            public bool isPreferredPlus { get; set; }
+        }
+
+        public class Pricebreakdown
+        {
+            public Grossprice grossPrice { get; set; }
+            public object[] taxExceptions { get; set; }
+            public Benefitbadge[] benefitBadges { get; set; }
+            public Strikethroughprice strikethroughPrice { get; set; }
+            public Excludedprice excludedPrice { get; set; }
+        }
+
+        public class Grossprice
+        {
+            public string currency { get; set; }
+            public float value { get; set; }
+        }
+
+        public class Strikethroughprice
+        {
+            public string currency { get; set; }
+            public float value { get; set; }
+        }
+
+        public class Excludedprice
+        {
+            public string currency { get; set; }
+            public float value { get; set; }
+        }
+
+        public class Benefitbadge
+        {
+            public string variant { get; set; }
+            public string explanation { get; set; }
+            public string identifier { get; set; }
+            public string text { get; set; }
+        }
+
+        public class Checkout
+        {
+            public string untilTime { get; set; }
+            public string fromTime { get; set; }
+        }
+
+        public class Checkin
+        {
+            public string untilTime { get; set; }
+            public string fromTime { get; set; }
+        }
+
+        public class Meta
+        {
+            public string title { get; set; }
+        }
+
+        public class Appear
+        {
+            public Component component { get; set; }
+            public string id { get; set; }
+            public string contentUrl { get; set; }
+        }
+
+        public class Component
+        {
+            public Props props { get; set; }
+        }
+
+        public class Props
+        {
+            public bool fill { get; set; }
+            public Content content { get; set; }
+            public string text { get; set; }
+            public string title { get; set; }
+        }
+
+        public class Content
+        {
+            public Props1 props { get; set; }
+        }
+
+        public class Props1
+        {
+            public Item[] items { get; set; }
+            public bool fitContentWidth { get; set; }
+        }
+
+        public class Item
+        {
+            public Props2 props { get; set; }
+        }
+
+        public class Props2
+        {
+            public Component1 component { get; set; }
+        }
+
+        public class Component1
+        {
+            public Props3 props { get; set; }
+        }
+
+        public class Props3
+        {
+            public Item1[] items { get; set; }
+            public string spacing { get; set; }
+            public string icon { get; set; }
+            public string variant { get; set; }
+            public string accessibilityLabel { get; set; }
+            public string tertiaryTintedColor { get; set; }
+        }
+
+        public class Item1
+        {
+            public Props4 props { get; set; }
+        }
+
+        public class Props4
+        {
+            public Text[] text { get; set; }
+        }
+
+        public class Text
+        {
+            public string text { get; set; }
+            public string font { get; set; }
+            public string color { get; set; }
+            public Linkaction[] linkActions { get; set; }
+        }
+
+        public class Linkaction
+        {
+            public Props5 props { get; set; }
+        }
+
+        public class Props5
+        {
+            public string url { get; set; }
+        }
+}
